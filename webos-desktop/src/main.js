9b5b315be218f76c2a6e069177d020e93dbb1e30
TheLp281 <efekantunc0@gmail.com>
2025-12-22 14:13:54 +0100

    Run build

--- webos-desktop/src/main.js
+++ webos-desktop/src/main.js
@@ -405,12 +405,12 @@ const appLauncher = new AppLauncher(
 const desktopUI = new DesktopUI(appLauncher);
 SystemUtilities.startClock();
 SystemUtilities.setRandomWallpaper();
-const queryString = window.location.search; 
+const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
-const game = urlParams.get('game'); 
-if(game) {
+const game = urlParams.get("game");
+if (game) {
   setTimeout(() => {
-    appLauncher.launch(game)
+    appLauncher.launch(game);
   }, 100);
 }
